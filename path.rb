eb6cde3518f10a2bc9f463a2074feb16646f289e
Jonathan O'Donnell <joncodo@gmail.com>
2020-07-16 13:59:01 -0300

    workign with tests and more features

--- path.rb
+++ path.rb
@@ -2,23 +2,17 @@ require 'csv'
 
 class Path
   
-  def read_file file_name
-    @csv = CSV.read(file_name)
-  end
-
-  def extract_history_sets count=4
-    
+  def extract_sets set_length=4
     sets_with_count = {}
-    # sets_with_count["p1,p2,p3,p4"] = 1123
 
     @csv.each do |line|
       count = 1
       key = ""
       line.each do |history|
         key += history
-        if count == 4
-          sets_with_count[key].nil? ?  sets_with_count[key] = 1 : sets_with_count[key] += 1
-          key = key[2..-1] # remove the first page to check the next set
+        if count == set_length
+          increment_hash_value sets_with_count, key
+          key = remove_first_key key
         else 
           count += 1
         end 
@@ -26,6 +20,18 @@ class Path
     end
 
     sets_with_count.max_by{|k,v| v}[0]
-
   end
+
+  def read_file file_name
+    @csv = CSV.read(file_name)
+  end
+
+  def increment_hash_value hash, key
+    hash[key].nil? ? hash[key] = 1 : hash[key] += 1
+  end
+
+  def remove_first_key key
+    key[2..-1]
+  end
+
 end
--- spec/path.spec.rb
+++ spec/path.spec.rb
@@ -5,21 +5,45 @@ class PathTest < Minitest::Test
   def test_read_file
     path = Path.new
     assert_equal path.read_file("paths.csv")[0], ["p9", "p3", "p6"]
+    assert_equal "p3p0p9p6", path.extract_sets
   end
 
-  def test_extract_history_sets
+  def test_extract_sets
     path = Path.new
     csv = path.read_file('simple_paths.csv')
-    assert_equal path.extract_history_sets, "p7p6p6p9"
+    assert_equal "p7p6p6p9", path.extract_sets
 
     path = Path.new
     csv = path.read_file('instructions.csv')
-    assert_equal path.extract_history_sets, "p2p3p2p3"
+    assert_equal "p2p3p2p3", path.extract_sets
 
     path = Path.new
     csv = path.read_file('instructions.csv')
-    assert_equal path.extract_history_sets(3), "p2p3p2p3"
+    assert_equal "p2p3p2", path.extract_sets(3)
+    
+    path = Path.new
+    csv = path.read_file('instructions.csv')
+    assert_equal "p2p3", path.extract_sets(2)
+
+    path = Path.new
+    csv = path.read_file('instructions.csv')
+    assert_equal "p2", path.extract_sets(1) 
   end
 
+  def test_increment_hash_value
+    path = Path.new
+    hash = {}
+    path.increment_hash_value hash, "foo"
+    assert_equal 1, hash["foo"]
+    path.increment_hash_value hash, "foo"
+    assert_equal 2, hash["foo"]
+  end
+
+  def test_remove_first_key
+    path = Path.new
+    hash = {}
+    new_key = path.remove_first_key "two"
+    assert_equal "o", new_key
+  end
 
 end
